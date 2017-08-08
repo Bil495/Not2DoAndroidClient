706e94b28dcddcd51969f4f21fde7ae54541dfc6
Görkem Mülayim <gorkemmulayim@icloud.com>
2017-08-08 12:11:53 +0300

    Fix parsing error in notification.

--- app/src/main/java/bil495/not2do/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/bil495/not2do/fcm/MyFirebaseMessagingService.java
@@ -18,9 +18,10 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     private static final String TAG = "FCM Service";
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        Log.d(TAG, "Notification Data: " + remoteMessage.getData().toString());
+        Log.d(TAG, "Notification Data: " + remoteMessage.getData().toString().split("\"")[1]);
 
         String body = remoteMessage.getData().toString();
+        body = body.split("\"")[1];
         MyNotificationManager notifier = new MyNotificationManager();
         notifier.notify(getSystemService(Context.NOTIFICATION_SERVICE), getBaseContext(), body);
     }
